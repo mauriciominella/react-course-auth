543a5cc216ac96e624f4cb611de355deb7dcf2f8
Mauricio Minella <mauriciominella@gmail.com>
2018-09-11 23:09:03 +0200

    Creating the auth_user action.

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
-
 class Signup extends Component {
+    onSubmit = formProps => {
+        console.log(formProps);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 <fieldset>
                     <label>Email</label>
                     <Field 
@@ -23,6 +28,7 @@ class Signup extends Component {
                         component="input"
                         autoComplete="none" />
                 </fieldset>
+                <button>Sign Up!</button>
             </form>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 
+const initialState = {};
+
+const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
         <BrowserRouter>
             <App>
                 <Route path="/" exact component={Welcome} />
