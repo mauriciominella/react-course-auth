e6b7efc8fa36f3e3f92625b12bb20a41bc93ce17
Mauricio Minella <mauriciominella@gmail.com>
2018-09-23 16:33:23 +0200

    Requiring authentication in the Feature component

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
 
 const initialState = {};
 
@@ -24,6 +25,7 @@ ReactDOM.render(
             <App>
                 <Route path="/" exact component={Welcome} />
                 <Route path="/signup" exact component={Signup} />
+                <Route path="/feature" exact component={Feature} />
             </App> 
         </BrowserRouter>
     </Provider>,
